fcdee102acfe62a0f748d7ef68f6dbba3e534885
Bernardo Castro and Gabriela Rodriguez <dev+bermanya+gabelula@cuboxsa.com>
2010-11-30 15:04:56 -0200

    Remove pq gem and failed migration

--- db/migrate/20101020185745_create_location_translation.rb
+++
@@ -1,9 +0,0 @@
-class CreateLocationTranslation < ActiveRecord::Migration
-  def self.up
-    Location.create_translation_table! :name => :string
-  end
-
-  def self.down
-    Location.drop_translation_table!
-  end
-end
