08a75ab5279a155d20d9a65180bbe72fba814c0b
Bernardo Castro <bernacas@gmail.com>
2010-12-03 18:39:53 -0200

    First step to upgrade to rails 3

--- config/environment.rb
+++ config/environment.rb
@@ -1,36 +1,5 @@
-# Be sure to restart your server when you modify this file
+# Load the rails application
+require File.expand_path('../application', __FILE__)
 
-# Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
-
-# Bootstrap the Rails environment, frameworks, and default configuration
-require File.join(File.dirname(__FILE__), 'boot')
-
-require "ostruct"
-
-Rails::Initializer.run do |config|
-  # Add additional load paths for your own custom dirs
-  # config.load_paths += %W( #{RAILS_ROOT}/extras )
-
-  # Only load the plugins named here, in the order given (default is alphabetical).
-  # :all can be used as a placeholder for all plugins not explicitly named
-  # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
-
-  # Skip frameworks you're not going to use. To use Rails without a database,
-  # you must remove the Active Record framework.
-  config.frameworks -= [ :active_resource ]
-
-  # Activate observers that should always be running
-  # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
-
-  # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-  # Run "rake -D time" for a list of tasks for finding time zone names.
-  config.time_zone = 'UTC'
-
-  config.action_controller.session = {
-    :session_key => 'natdatcat',
-    :secret      => 'f3f57b71ef9345ffccd0c4e841d8e74bb2e7d2ef692a506aa6c2a3c29d584a55dd18426ffc04610be49956a51af'
-  }
-
-  config.middleware.use "Rack::Honeypot"
-end
+# Initialize the rails application
+DatacatalogWeb::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DatacatalogWeb::Application.config.secret_token = '0ce24e311814a59560bb307f114753dfa8e58b8a8a9a103469b808c8c4ee80a3da4f672768118172faaea6605167b53c87275f893d75b3ec7147c25479cbc3f2'
