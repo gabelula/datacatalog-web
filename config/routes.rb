7565d5737d2fb5b68f32c236d0341a978c3f07ca
Bernardo Castro <bernacas@gmail.com>
2010-12-06 15:29:29 -0200

    Upgrade routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,51 +1,46 @@
-ActionController::Routing::Routes.draw do |map|
-  map.resources :data_records, :as => "data", :except => :destroy do |data_record|
-    data_record.resource :favorite, :only => [:create, :destroy]
-    data_record.resource :rating, :only => [:update]
-    data_record.resources :notes, :only => [:index, :create]
-    data_record.resources :comments, :only => [:create] do |comments|
-      comments.resources :votes, :only => [:create]
+DatacatalogWeb::Application.routes.draw do
+  resources :data_records do
+    resource :favorite
+    resource :rating
+    resources :notes
+    resources :comments do
+      resources :votes
     end
-    data_record.resource :wiki, :as => "docs", :only => [:show, :edit, :update]
+    resource :wiki
   end
 
-  map.resources :organizations, :as => :org, :only => [:show]
-
-  map.namespace :admin do |admin|
-    admin.resources :organizations, :only => [:index, :show, :new, :create, :update]
-    admin.resources :data_records, :only => [:index]
-    admin.resources :users, :only => [:index, :show, :new, :create, :update]
-    admin.resources :contact_submissions, :only => [:index, :show, :update]
+  resources :organizations
+  namespace :admin do
+    resources :organizations
+    resources :data_records
+    resources :users
+    resources :contact_submissions
   end
 
-  map.resources :users, :only => [:new, :create]
-  map.resource :profile, :controller => "users", :only => [:edit, :update]
-  map.resource :user_session
-
-  map.root                                                        :controller => "main",            :action => "dashboard"
-  map.about              "about",                                 :controller => "main",            :action => "about"
-  map.blog               "blog",                                  :controller => "main",            :action => "blog"
-  map.confirm            "confirm/:token",                        :controller => "users",           :action => "confirm"
-  map.contact            "contact",                               :controller => "contact",         :action => "index"
-  map.contact_submission "contact/submit",                        :controller => "contact",         :action => "submit"
-  map.dashboard          "dashboard",                             :controller => "main",            :action => "dashboard"
-  map.forgot             "forgot",                                :controller => "password_resets", :action => "new"
-  map.perform_reset      "reset/attempt",                         :controller => "password_resets", :action => "update"
-  map.reset              "reset/:token",                          :controller => "password_resets", :action => "edit"
-  map.search             "search",                                :controller => "search",          :action => "index"
-  map.send_reset         "forgot/sent",                           :controller => "password_resets", :action => "create"
-  map.signin             "signin",                                :controller => "user_sessions",   :action => "new"
-  map.signout            "signout",                               :controller => "user_sessions",   :action => "destroy"
-  map.signup             "signup",                                :controller => "users",           :action => "new"
-  map.source             "data/:slug",                            :controller => "data",            :action => "show"
-  map.source_create_doc  "data/:slug/docs/create",                :controller => "data",            :action => "create_doc"
-  map.source_docs        "data/:slug/docs",                       :controller => "data",            :action => "docs"
-  map.source_edit_docs   "data/:slug/docs/edit",                  :controller => "data",            :action => "edit_docs"
-  map.source_show_doc    "data/:slug/docs/:id",                   :controller => "data",            :action => "show_doc"
-  map.source_update_doc  "data/:slug/docs/:id/update",            :controller => "data",            :action => "update_doc"
-  map.source_usages      "data/:slug/usages",                     :controller => "data",            :action => "usages"
-
-  map.connect ":controller/:action/:id"
-  map.connect ":controller/:action/:id.:format"
-
+  resources :users
+  resource :profile
+  resource :user_session
+  match '/' => 'main#dashboard'
+  match 'about' => 'main#about', :as => :about
+  match 'blog' => 'main#blog', :as => :blog
+  match 'confirm/:token' => 'users#confirm', :as => :confirm
+  match 'contact' => 'contact#index', :as => :contact
+  match 'contact/submit' => 'contact#submit', :as => :contact_submission
+  match 'dashboard' => 'main#dashboard', :as => :dashboard
+  match 'forgot' => 'password_resets#new', :as => :forgot
+  match 'reset/attempt' => 'password_resets#update', :as => :perform_reset
+  match 'reset/:token' => 'password_resets#edit', :as => :reset
+  match 'search' => 'search#index', :as => :search
+  match 'forgot/sent' => 'password_resets#create', :as => :send_reset
+  match 'signin' => 'user_sessions#new', :as => :signin
+  match 'signout' => 'user_sessions#destroy', :as => :signout
+  match 'signup' => 'users#new', :as => :signup
+  match 'data/:slug' => 'data#show', :as => :source
+  match 'data/:slug/docs/create' => 'data#create_doc', :as => :source_create_doc
+  match 'data/:slug/docs' => 'data#docs', :as => :source_docs
+  match 'data/:slug/docs/edit' => 'data#edit_docs', :as => :source_edit_docs
+  match 'data/:slug/docs/:id' => 'data#show_doc', :as => :source_show_doc
+  match 'data/:slug/docs/:id/update' => 'data#update_doc', :as => :source_update_doc
+  match 'data/:slug/usages' => 'data#usages', :as => :source_usages
+  match '/:controller(/:action(/:id))'
 end
