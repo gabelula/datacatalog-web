f9eb631a4df8ba5f034218c617eddd6a11d267f9
Gaba <gaba@cuboxsa.com>
2010-12-10 17:18:58 -0200

    Updated authlogic for rails 3

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,7 +1,7 @@
 class UserSessionsController < ApplicationController
+  before_filter :activate_authlogic
   before_filter :require_no_user, :only => [:new, :create]
   before_filter :require_user, :only => :destroy
-  before_filter :activate_authlogic
 
   def new
     @user_session = UserSession.new
