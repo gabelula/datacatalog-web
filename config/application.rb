815396bc6e058d774c87716118da7072ea0247cd
Gaba <gaba@cuboxsa.com>
2010-12-10 17:23:52 -0200

    Changed config to the rails 3 way.

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'ostruct'
 
 # If you have a Gemfile, require the gems listed there, including any gems
 # you've limited to :test, :development, or :production.
@@ -9,7 +10,7 @@ Bundler.require(:default, Rails.env) if defined?(Bundler)
 module DatacatalogWeb
   class Application < Rails::Application
     # Add additional load paths for your own custom dirs
-    # config.load_paths += %W( #{RAILS_ROOT}/extras )
+    config.autoload_paths += %W( #{Rails.root}/lib)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named
@@ -23,10 +24,14 @@ module DatacatalogWeb
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names.
+    config.time_zone = 'UTC'
 
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file
     config.filter_parameters += [:password]
+
+    config.middleware.use "Rack::Honeypot"
+    
   end
 end
